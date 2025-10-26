56de4f54df65bb151b5acc5831a355a9dde9c0d4
Roman Ivanytskyi <rizvanua@gmail.com>
2025-10-26 19:22:48 +0200

    nest js

--- apps/backend/amplify/functions/nest/src/app.module.ts
+++ apps/backend/amplify/functions/nest/src/app.module.ts
@@ -1,7 +1,8 @@
 // amplify/functions/nest/src/app.module.ts
 import { Module } from "@nestjs/common";
 import { HealthController } from "./health.controller";
+import { MainController } from "./main.controller";
 // import { UsersController } from "./users.controller";
 
-@Module({ controllers: [HealthController] })
+@Module({ controllers: [HealthController, MainController] })
 export class AppModule {}
